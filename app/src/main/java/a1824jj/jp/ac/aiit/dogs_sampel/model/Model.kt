5ee3ca6c70eda2f2eb4e6a7ec78bac9008747dab
a1824jj <a1824jj@aiit.ac.jp>
2020-04-20 13:39:07 +0900

    add DogBreed - model

---
+++ app/src/main/java/a1824jj/jp/ac/aiit/dogs_sampel/model/Model.kt
@@ -0,0 +1,11 @@
+package a1824jj.jp.ac.aiit.dogs_sampel.model
+
+data class DogBreed(
+    val breedId: String?,
+    val dogBreed: String?,
+    val lifeSpan: String?,
+    val breedGroup: String?,
+    val bredFor: String?,
+    val temperament: String?,
+    val imageUrl: String?
+)
